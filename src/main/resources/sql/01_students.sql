d4947044f229af9d6cf6a883cdba7749033697a6
ddliashenko <d.d.lyashenko@gmail.com>
2023-08-20 19:33:04 +0700

    ATA-400: add test data for students and change DB to postgreSQL (#7)
    ATA-400: add test data for students and change DB to postgreSQL

---
+++ src/main/resources/sql/01_students.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (id, name, surname, grade_number, phone_number)
+VALUES
+(nextval('student_seq'), 'Вася', 'Сидоров', '10a', '+79111435676'),
+(nextval('student_seq'), 'Маша', 'Косичкина', '10a', '+79111321456'),
+(nextval('student_seq'), 'Настя', 'Лабанова', '10a', '+79111123452');
---
+++ src/main/resources/sql/04_vasya.sql
@@ -0,0 +1,49 @@
+--Вася. Биология
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 3, '2023-08-10' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 2, '2023-08-11' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 2, '2023-08-12' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 5, '2023-08-13' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 3, '2023-08-14' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 2, '2023-08-15' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 3, '2023-08-16' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Биология', 3, '2023-08-17' FROM student WHERE NAME = 'Вася';
+
+--Вася. Математика
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 3, '2023-08-10' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 2, '2023-08-11' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 2, '2023-08-12' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 5, '2023-08-13' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 3, '2023-08-14' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 2, '2023-08-15' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 3, '2023-08-16' FROM student WHERE NAME = 'Вася';
+
+INSERT INTO assesment (id, student_id, subject, score, date)
+SELECT nextval('student_seq'), id, 'Математика', 3, '2023-08-17' FROM student WHERE NAME = 'Вася';
